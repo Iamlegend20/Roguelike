64c12962864573d8e2cc08b6bcad4eb9b7fc21b1
Iamlegend20 <maloy8920@gmail.com>
2022-09-11 11:53:31 -0400

    Implemented pynput for movement.

--- roguelike.py
+++ roguelike.py
@@ -1,5 +1,6 @@
 import os
 import time
+from pynput import keyboard
 class Grid:
 
     def __init__(self, x_length, y_length, z_length):
@@ -70,58 +71,72 @@ class Character:
     def move(self, grid, direction):
         current_tile = grid.grid[self.x_pos][self.y_pos][self.z_pos].occupants
         
-        if direction == 4:
+        if direction == '<100>':
             grid.grid[self.x_pos-1][self.y_pos][self.z_pos].occupants.append(self)
             self.x_pos -= 1
-        elif direction == 6:
+        elif direction == '<102>':
             grid.grid[self.x_pos+1][self.y_pos][self.z_pos].occupants.append(self)
             self.x_pos += 1
-        elif direction == 8:
+        elif direction == '<104>':
             grid.grid[self.x_pos][self.y_pos+1][self.z_pos].occupants.append(self)
             self.y_pos += 1
-        elif direction == 2:
+        elif direction == '<98>':
             grid.grid[self.x_pos][self.y_pos-1][self.z_pos].occupants.append(self)
             self.y_pos -= 1
-        elif direction == 7:
+        elif direction == '<103>':
             grid.grid[self.x_pos-1][self.y_pos+1][self.z_pos].occupants.append(self)
             self.x_pos -= 1
             self.y_pos += 1
-        elif direction == 9:
+        elif direction == '<105>':
             grid.grid[self.x_pos+1][self.y_pos+1][self.z_pos].occupants.append(self)
             self.x_pos += 1
             self.y_pos += 1
-        elif direction == 1:
+        elif direction == '<97>':
             grid.grid[self.x_pos-1][self.y_pos-1][self.z_pos].occupants.append(self)
             self.x_pos -= 1
             self.y_pos -= 1
-        elif direction == 3:
+        elif direction == '<99>':
             grid.grid[self.x_pos+1][self.y_pos-1][self.z_pos].occupants.append(self)
             self.x_pos += 1
-            self.y_pos -= 1    
+            self.y_pos -= 1
+        elif direction == '<101>':
+            return     
 
         for occupant in current_tile:
             if occupant == self:
                 current_tile.remove(self)
                 break   
 
-def get_input():
-    accepted_input = ("1","2","3","4","6","7","8","9")
-    player_input = input()
-    while player_input not in accepted_input:
-        player_input = input()
-    return int(player_input)
+def validate_input(player_input):
+    global key_pressed
+    accepted_input = ("<97>","<98>","<99>","<100>","<101>","<102>","<103>","<104>","<105>")
+    if player_input not in accepted_input:
+        return False
 
+def on_press(key):
+    global player_input
+    global key_pressed
+    player_input = str(key)
+    key_pressed = True
 
+player_input = None
+key_pressed = False
 def main():
+    global player_input
+    global key_pressed
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
     grid = Grid(60,20,1)
     player = Character(2,2,0)
     player.spawn(grid)
     grid.update()
-
+    
     while True:
-        player_input = get_input()
-        player.move(grid, player_input)
-        grid.update()
+        while key_pressed:
+            if validate_input(player_input) == False : break
+            player.move(grid, direction=player_input)
+            grid.update()
+            key_pressed = False
 
 if __name__ == '__main__':
     main()
